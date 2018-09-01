bc1d360ddf89f4ad6ab627ec77a3bd7ab0bd1de7
frootima <frootima@gmail.com>
2018-09-01 04:01:56 +0000

    mybaties_add

--- scripts/20180901034426_DB_files.sql
+++ scripts/20180901034426_DB_files.sql
@@ -91,18 +91,6 @@ CREATE TABLE salaries (
     PRIMARY KEY (emp_no, from_date)
 );
 
-SELECT 'LOADING departments' as 'INFO';
-source load_departments.dump ;
-SELECT 'LOADING employees' as 'INFO';
-source load_employees.dump ;
-SELECT 'LOADING dept_emp' as 'INFO';
-source load_dept_emp.dump ;
-SELECT 'LOADING dept_manager' as 'INFO';
-source load_dept_manager.dump ;
-SELECT 'LOADING titles' as 'INFO';
-source load_titles.dump ;
-SELECT 'LOADING salaries' as 'INFO';
-source load_salaries.dump ;
 
 
 
